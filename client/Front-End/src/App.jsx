6ea0a4b90c918dfbc2e59e94c683fcc83be5a8ea
AbdalrahmanAlzaro <abdalrahman.h.alzaro@gmail.com>
2024-02-23 15:38:16 +0300

    create a project

---
+++ client/Front-End/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline-offset-8 flex justify-center text-red-500 mt-[15rem]">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
