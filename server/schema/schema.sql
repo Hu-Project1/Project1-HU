5bce1a8c5fd80832195c9761402231317a4e9382
AbdalrahmanAlzaro <abdalrahman.h.alzaro@gmail.com>
2024-02-28 19:16:56 +0300

    push

---
+++ server/schema/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS public."user"
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    username character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    role character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT user_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."user"
+    OWNER to postgres;
+
+ALTER TABLE public."user"
+ADD COLUMN state boolean NOT NULL DEFAULT false;
